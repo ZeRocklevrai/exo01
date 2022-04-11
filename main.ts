22ab6a1a3d11268aa6d418ab6e519c6fe62cd3c1
ZeRocklevrai <103418883+ZeRocklevrai@users.noreply.github.com>
2022-04-11 11:36:08 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 basic.forever(function () {
 	
 })
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("Hello!")
+        basic.showNumber(input.temperature())
+        basic.showString("Hello!")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
