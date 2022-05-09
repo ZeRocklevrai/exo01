f22ec09527a54535edf7c8d1d4640c3b25e72436
ZeRocklevrai <103418883+ZeRocklevrai@users.noreply.github.com>
2022-05-09 11:06:06 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,51 @@
 basic.forever(function () {
-	
-})
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.clearScreen()
-        basic.showString("Hello!")
-        basic.showNumber(input.temperature())
-        basic.showString("Hello!")
-    } else {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
